b9ea5a6759de4700b46457d4a8ba615300018d49
Thielson Almendra <thielson12@gmail.com>
2023-04-22 18:00:25 -0300

    feat: finish button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,12 +1,18 @@
+import React from 'react'
 import { TouchableOpacity, Text, TouchableOpacityProps } from 'react-native'
+import { Feather } from '@expo/vector-icons'
+import { styled } from 'nativewind'
+
+const StyledIcon = styled(Feather)
 
 type ButtonProps = {
-  text: string
+  title?: string
   size?: 'small' | 'medium' | 'large'
+  iconName?: React.ComponentProps<typeof Feather>['name']
 } & TouchableOpacityProps
 
-function Button({ text, size, ...props }: ButtonProps) {
-  const className = !size
+function Button({ title, size, iconName, className, ...props }: ButtonProps) {
+  const classSize = !size
     ? 'w-full px-[20px] py-[12px]'
     : size === 'small'
     ? 'px-[16px] py-[8px]'
@@ -16,16 +22,21 @@ function Button({ text, size, ...props }: ButtonProps) {
 
   return (
     <TouchableOpacity
-      className={`${className} bg-produto-greenLight rounded-[6px] items-center`}
+      className={`${classSize} bg-produto-greenLight rounded-[6px] items-center ${className}`}
       {...props}
     >
-      <Text
-        className={`text-base-white ${
-          size === 'small' ? 'text-style-bold-sm' : 'text-style-bold-md'
-        }`}
-      >
-        {text}
-      </Text>
+      {title && (
+        <Text
+          className={`text-base-white ${
+            size === 'small' ? 'text-style-bold-sm' : 'text-style-bold-md'
+          }`}
+        >
+          {title}
+        </Text>
+      )}
+      {iconName && (
+        <StyledIcon name={iconName} size={30} className="text-base-white" />
+      )}
     </TouchableOpacity>
   )
 }
