edadeb432edd4d0c0213f76084565cb03b30c02b
Thielson Almendra <thielson12@gmail.com>
2023-04-23 23:42:23 -0300

    feat: add controled input

---
+++ src/components/Inputs/ControledInputText/index.tsx
@@ -0,0 +1,115 @@
+import { UseControllerProps, useController } from 'react-hook-form'
+import { TextInput, TextInputProps, View, Text } from 'react-native'
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+  interpolate,
+  interpolateColor,
+} from 'react-native-reanimated'
+import { colors } from '../../../theme/theme'
+import { useState } from 'react'
+
+type ControledInputTextProps<T> = {
+  label: string
+  placeholder: string
+  hintText?: string
+  width?: number
+  required?: boolean
+  controlProps?: UseControllerProps<T>
+} & TextInputProps
+
+const TextAnimated = Animated.createAnimatedComponent(Text)
+const TextInputAnimated = Animated.createAnimatedComponent(TextInput)
+
+function ControledInputText<T>({
+  label,
+  placeholder,
+  hintText,
+  width,
+  required,
+  controlProps,
+  ...props
+}: ControledInputTextProps<T>) {
+  const {
+    field,
+    fieldState: { error },
+  } = useController(controlProps)
+
+  const [isFocused, setIsFocused] = useState(false)
+
+  const selectedOffset = useSharedValue(error?.message ? 1 : 0)
+  const focusOffset = useSharedValue(0)
+
+  const errorMessageAnimatedStyle = useAnimatedStyle(() => {
+    selectedOffset.value = error?.message
+      ? withTiming(1, { duration: 150 })
+      : withTiming(0, { duration: 150 })
+    return {
+      fontSize: interpolate(selectedOffset.value, [0, 1], [0, 14]),
+    }
+  }, [error])
+
+  const inputAnimatedStyle = useAnimatedStyle(() => {
+    console.log(focusOffset.value)
+    focusOffset.value = isFocused
+      ? withTiming(1, { duration: 150 })
+      : withTiming(0, { duration: 150 })
+    return {
+      borderColor: interpolateColor(
+        focusOffset.value,
+        [0, 1],
+        [
+          error?.message ? colors.produto.redDark : '#00000000',
+          error?.message ? colors.produto.redDark : colors.produto.greenLight,
+        ],
+      ),
+    }
+  }, [isFocused, error])
+
+  return (
+    <View style={{ gap: 4, width, flex: width ? undefined : 1 }}>
+      {label && (
+        <View className="flex-row items-center" style={{ gap: 4 }}>
+          <Text className="text-style-regular-md text-base-white">
+            {label + ':'}
+          </Text>
+          {required && (
+            <Text className="text-style-regular-xs text-base-placeholder">
+              {'(Obrigatório)'}
+            </Text>
+          )}
+        </View>
+      )}
+      <TextInputAnimated
+        className="w-full rounded-[6px] bg-base-background px-4 py-3 text-style-regular-md text-base-white"
+        style={[
+          {
+            borderWidth: 1,
+          },
+          inputAnimatedStyle,
+        ]}
+        placeholder={placeholder}
+        placeholderTextColor="#7C7C8A"
+        value={field?.value as string}
+        onChangeText={field?.onChange}
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
+        {...props}
+      />
+      {hintText && (
+        <Text className="texxt-style-regular-sm text-base-placeholder">
+          {hintText}
+        </Text>
+      )}
+      <TextAnimated
+        className="texxt-style-regular-sm text-produto-redDark align-text-top"
+        style={errorMessageAnimatedStyle}
+      >
+        {error?.message}
+      </TextAnimated>
+    </View>
+  )
+}
+
+export default ControledInputText
--- src/components/Modal/BottomSheetCreateTransaction/index.tsx
+++ src/components/Modal/BottomSheetCreateTransaction/index.tsx
@@ -14,6 +14,11 @@ import InputText from '../../Inputs/InputText'
 import { useRef, useState } from 'react'
 import SelectTypeTransaction from '../../Selects/SelectType'
 import SelectMonthly from '../../Selects/SelectMonthly'
+import { useForm } from 'react-hook-form'
+import { TransactionForm } from '../../../Types/transaction'
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
+import ControledInputText from '../../Inputs/ControledInputText'
 
 type BottomSheetCreateTransactionProps = {
   showModal: boolean
@@ -22,12 +27,25 @@ type BottomSheetCreateTransactionProps = {
 
 const SCREEN_HEIGHT_WITH_STATUSBAR = Dimensions.get('screen').height
 
+const schema = yup.object().shape({
+  description: yup.string().required('Descrição é obrigatária!'),
+})
+
 function BottomSheetCreateTransaction({
   showModal,
   onClose,
 }: BottomSheetCreateTransactionProps) {
   const marginContent = useRef(0)
   const [contantHeight, setContentHeight] = useState(0)
+  const { control, handleSubmit } = useForm<TransactionForm>({
+    resolver: yupResolver(schema),
+    defaultValues: {
+      description: '',
+    },
+  })
+  function handleSubmitForm(data: TransactionForm) {
+    console.log(data)
+  }
 
   return (
     <Modal
@@ -65,7 +83,11 @@ function BottomSheetCreateTransaction({
                 <Close width={24} height={24} />
               </TouchableOpacity>
             </View>
-            <InputText
+            <ControledInputText
+              controlProps={{
+                control,
+                name: 'description',
+              }}
               label="Descrição"
               placeholder="Descrição da transação"
               required
@@ -75,17 +97,36 @@ function BottomSheetCreateTransaction({
               style={{ gap: 8 }}
             >
               <InputText
+                // controlProps={{ control, name: 'valueTransaction' }}
                 label="Valor da transação"
                 placeholder="R$ 0,00"
                 required
               />
-              <InputText label="Vezes" placeholder="1" width={100} />
+              <InputText
+                // controlProps={{ control, name: 'times' }}
+                label="Vezes"
+                placeholder="1"
+                width={100}
+              />
             </View>
-            <InputText label="Categoria" placeholder="Categoria" required />
+            <InputText
+              // controlProps={{ control, name: 'category' }}
+              label="Categoria"
+              placeholder="Categoria"
+              required
+            />
             <SelectTypeTransaction required />
             <SelectMonthly disabled={false} required />
-            <InputText label="Data" placeholder="dd/mm/aaaa" />
-            <Button title="Cadastrar" size="large" />
+            <InputText
+              // controlProps={{ control, name: 'date' }}
+              label="Data"
+              placeholder="dd/mm/aaaa"
+            />
+            <Button
+              title="Cadastrar"
+              size="large"
+              onPress={handleSubmit(handleSubmitForm)}
+            />
           </View>
         </ScrollView>
       </KeyboardAvoidingView>
